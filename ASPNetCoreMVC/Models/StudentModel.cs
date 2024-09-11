624f9b9978d7865377c0199084324b08bd721697
Qasim-Akbar <qasim.akbar343@gmail.com>
2024-09-11 10:27:49 +0500

    Student Model class added with properties

---
+++ ASPNetCoreMVC/Models/StudentModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ASPNetCoreMVC.Models
+{
+    public class StudentModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
